8666b0826e8e2cbe2ec2affe94bcc5efb5c94ef4
yingyedeng <woahydeng@gmail.com>
2022-03-24 10:13:36 -0400

    header

---
+++ vite-project/array.js
@@ -0,0 +1,49 @@
+const cities = [
+    {
+        City: "London",
+        Country: "United Kingdom",
+        Image: "https://cdn.britannica.com/01/94501-050-7C939333/Big-Ben-London.jpg",
+    },
+    {
+        City: "Paris",
+        Country: "France",
+        Image: "",
+    },
+    {
+        City: "",
+        Country: "",
+        Image: "",
+    },
+    {
+        City: "",
+        Country: "",
+        Image: "",
+    },
+    {
+        City: "",
+        Country: "",
+        Image: "",
+    },
+    {
+        City: "",
+        Country: "",
+        Image: "",
+    },
+    {
+        City: "",
+        Country: "",
+        Image: "",
+    },
+    {
+        City: "",
+        Country: "",
+        Image: "",
+    },
+    {
+        City: "",
+        Country: "",
+        Image: "",
+    },
+];
+
+export { cities };
--- vite-project/main.js
+++ vite-project/main.js
@@ -1,1 +1,14 @@
 import './style.css'
+import { cities } from "./array";
+
+function makeCards() {
+    cities.forEach((cities) => {
+        document.querySelector(".cities").insertAdjacentHTML(
+            "afterbegin",
+            `
+            <div class="cityCard">
+            </div>
+            `
+        )
+    })
+}
