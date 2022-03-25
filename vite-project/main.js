5e2c8ac30340ae1a39f08dafcc96034f5e7ba655
yingyedeng <woahydeng@gmail.com>
2022-03-25 10:14:11 -0400

    display cards

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,14 +1,28 @@
 import './style.css'
 import { cities } from "./array";
 
+console.log(cities)
+
+let sortedCities = cities.sort(function(a, b) {
+    return a.id - b.id;
+})
+.reverse();
+console.log(sortedCities)
+
 function makeCards() {
-    cities.forEach((cities) => {
+    sortedCities.forEach((cities) => {
         document.querySelector(".cities").insertAdjacentHTML(
             "afterbegin",
             `
             <div class="cityCard">
+                <img class="cityImage" 
+                    src=${cities.Image}
+                </a>
+                <h2 class="cityName">${cities.City}</h2>
+                <h2 class="cityCountry">${cities.Country}</h2>
             </div>
             `
         )
-    })
+    });
 }
+makeCards();
